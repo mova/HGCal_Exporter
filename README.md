39eb24b31e156c9d309560a55d0622dc69c1d079
SohamBhattacharya <soham.elessar@gmail.com>
2021-01-05 18:03:24 +0100

    Minor changes.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Readme...
+```
+cmsrel CMSSW_11_1_6
+cd CMSSW_11_1_6/src/
+git clone git@github.com:SohamBhattacharya/HGCal_GAN.git .
+scram b -j8 USER_CXXFLAGS="-fopenmp"
+./run.sh
+```
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# If on DESY NFS, use inputFiles:/nfs/dust/cms/user/korcariw/generation/Guns/eGun10k/step3.root
+
 cmsRun EDAnalyzers/TreeMaker/python/ConfFile_cfg.py \
     inputFiles=/afs/cern.ch/work/w/wkorcari/public/eGun10k/step3.root \
     genEleFilter=0 \
