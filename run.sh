521e1e0ae8cecdcb0e71a679279c4c61a695c69f
SohamBhattacharya <soham.elessar@gmail.com>
2021-01-05 18:04:52 +0100

    Minor changes.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-# If on DESY NFS, use inputFiles:/nfs/dust/cms/user/korcariw/generation/Guns/eGun10k/step3.root
+# If on DESY NFS, use inputFiles=/nfs/dust/cms/user/korcariw/generation/Guns/eGun10k/step3.root
 
 cmsRun EDAnalyzers/TreeMaker/python/ConfFile_cfg.py \
     inputFiles=/afs/cern.ch/work/w/wkorcari/public/eGun10k/step3.root \
