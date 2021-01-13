59274ebbcba397b8e753601eab43e1644e4da815
mova <mova@users.noreply.github.com>
2021-01-13 11:29:20 +0100

    run.sh: local file, single event

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # If on DESY NFS, use inputFiles=/nfs/dust/cms/user/korcariw/generation/Guns/eGun10k/step3.root
-
+set -xe
 cmsRun EDAnalyzers/TreeMaker/python/ConfFile_cfg.py \
-    inputFiles=/afs/cern.ch/work/w/wkorcari/public/eGun10k/step3.root \
+    inputFiles=/nfs/dust/cms/user/mscham/ganExampleData/step3.root \
     genEleFilter=0 \
     genPartonFilter=0 \
     isGunSample=1 \
@@ -9,4 +9,4 @@ cmsRun EDAnalyzers/TreeMaker/python/ConfFile_cfg.py \
     storeSimHit=1 \
     storeRecHit=1 \
     debugFile=0 \
-    maxEvents=100 \
+    maxEvents=1 \
