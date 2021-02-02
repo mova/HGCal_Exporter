9bdc004fe209f2ab4cc51356a65c77e3cc0878b5
mova <mova@users.noreply.github.com>
2021-02-02 11:41:01 +0100

    fix plots

--- treeReader.py
+++ treeReader.py
@@ -7,7 +7,7 @@ import pandas as pd
 import awkward as ak
 import matplotlib.pyplot as plt
 
-fn = "~/CMSSW_11_1_6/src/ntupleTree-re.root"
+fn = "~/CMSSW_11_1_6/src/ntupleTree.root"
 rf = uproot.open(fn)
 #%%
 for key in rf["/treeMaker;1/tree;1"].keys():
@@ -25,26 +25,25 @@ def ga(vars: List[str], eventNumber: int = 0, library: str = "np") -> np.ndarray
     return outpd 
 #%%
 arr=ga(['recHit_y','recHit_x','recHit_z','recHit_layer'])
-fig1, ((a1,a2),(a3,a4)) = plt.subplots(ncols=2, nrows=2)
-axes=[a1,a2,a3,a4]
-print(type(f1_axes[0]))
+#%%
+fig1, axes = plt.subplots(ncols=1, nrows=10)
+stretch=5
+fig1.set_size_inches(1*stretch, 10*stretch)
 
-for layer in range(4):
+for layer,ax in enumerate(axes):
     sel=arr[arr['recHit_layer']==layer]
-    axes[layer].scatter(arr['recHit_x'],arr['recHit_y'])
-    axes[layer].set_title(f'Layer {layer}')
-
-
+    ax.scatter(arr['recHit_x'],arr['recHit_y'])
+    ax.set_title(f'Layer {layer}')
+fig1.tight_layout()
+fig1.show()
 
 # %%
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
-ax.scatter(xs=ga("recHit_y"), ys=ga("recHit_z"), zs=ga("recHit_x"))
+
+ax.scatter(xs=arr["recHit_y"], ys=arr["recHit_z"], zs=arr["recHit_x"])
 ax.set_xlabel("X Label")
 ax.set_ylabel("Y Label")
 ax.set_zlabel("Z Label")
-plt.savefig("firstLight.png")
-# %%
-plt.hist(ga("recHit_z"))
 # %%
